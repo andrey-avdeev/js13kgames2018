0e3bb235bad306055b552a4a1040fe68f5d3b2f3
avdeev-andrew <seorazer@gmail.com>
2018-08-29 09:39:14 +0300

    sprites were resized

--- src/config.ts
+++ src/config.ts
@@ -9,20 +9,20 @@ export class Config {
   public static PLAYER_HORIZONTAL_SPEED: number = 4;
   public static PLAYER_JUMP_SPEED: number = 15;
   public static PLAYER_POSITION_REFRESH_FACTOR: number = 5;
-  public static PLAYER_BASE_WIDTH: number = 26;
-  public static PLAYER_BASE_HEIGHT: number = 26;
+  public static PLAYER_BASE_WIDTH: number = 24;
+  public static PLAYER_BASE_HEIGHT: number = 24;
   public static PLAYER_LIVES: number = 3;
 
   public static PLATFORM_LIVE_GAP: 2;
-  public static PLATFORM_BASE_WIDTH: number = 45;
-  public static PLATFORM_BASE_HEIGHT: number = 14;
+  public static PLATFORM_BASE_WIDTH: number = 38;
+  public static PLATFORM_BASE_HEIGHT: number = 12;
   public static PLATFORM_POOL_MAX_SIZE: number = 32;
   public static PLATFORM_AFTERJUMP_SPEED: number = 13;
   public static PLATFORM_EXPLOSION_PULSE_TIMEOUT: number = 200;
 
   public static ENEMY_LIVE_GAP: 2;
-  public static ENEMY_BASE_WIDTH: number = 26;
-  public static ENEMY_BASE_HEIGHT: number = 26;
+  public static ENEMY_BASE_WIDTH: number = 24;
+  public static ENEMY_BASE_HEIGHT: number = 24;
   public static ENEMY_POOL_MAX_SIZE: number = 20;
   public static ENEMY_MIN_SPEED: number = 2
   public static ENEMY_SPEED: number = 2;
